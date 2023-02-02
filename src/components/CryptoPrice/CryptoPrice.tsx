dc83c386e23065296543f546f8644acd7a4c393e
mnl187 <mnl187@gmail.com>
2023-02-02 06:58:18 +0100

    dodanie funkcji CryptoPrice

---
+++ src/components/CryptoPrice/CryptoPrice.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const CryptoPrice = () => {
+    return <>
+        <label htmlFor="">
+            Wybierz pare kryptowalutową: <br/>
+            <select name="" id="">
+
+            </select>
+        </label>
+    </>
+}
